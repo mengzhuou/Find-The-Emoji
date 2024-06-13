08b506d322ba016057064bfb2666bad4e85f65a6
mengzhuou <mou30@gatech.edu>
2024-06-12 21:47:58 -0400

    added bg image

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import background from './background/b1.jpeg'
+
+const emojis = ['😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃'];
+
 
 function App() {
   return (
     <div className="App">
+      <img className="background_img" src={background} alt="Background" />
       <header className="App-header">
         <p>
           Emoji App
+          {String.fromCodePoint(0x1F600)}
         </p>
       </header>
     </div>
